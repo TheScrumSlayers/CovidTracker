673f10fc5a8f93ed227ff11701afd5cfec5a4202
Gitspathe <55875553+Gitspathe@users.noreply.github.com>
2021-09-15 02:08:24 +1000

    Added more example data

--- WebApp/Code/Database/DatabaseHelper.cs
+++ WebApp/Code/Database/DatabaseHelper.cs
@@ -55,21 +55,19 @@ namespace CovidTracker.Code.Database
         /// <returns>IOReturn object, which may fail if the user cannot be found.</returns>
         public static async Task<IOReturn<User>> RetrieveUser(int userID)
         {
+            // First, try to retrieve the user from the local database.
+            IOReturn<User> ret = await RetrieveLocallyStoredUser(userID);
+            if (ret.Status == IOReturnStatus.Success) {
+                return ret;
+            }
+
             using (IServiceScope scope = Program.AppHost.Services.CreateScope()) {
                 IServiceProvider services = scope.ServiceProvider;
-
                 DatabaseContext context = services.GetRequiredService<DatabaseContext>();
 
-                // First, try to retrieve the user from the local database.
-                User user = await context.Users.FindAsync(userID);
-
-                // If the user is found locally, simply return it.
-                if (user != null)
-                    return new IOReturn<User>(IOReturnStatus.Success, user);
-
-                // Otherwise, try to retrieve the user from the external server.
+                // Otherwise if that fails, try to retrieve the user from the external server.
                 ExternalDatabaseContext externalContext = services.GetRequiredService<ExternalDatabaseContext>();
-                user = await externalContext.Users.FindAsync(userID);
+                User user = await externalContext.Users.FindAsync(userID);
 
                 if (user == null)
                     return new IOReturn<User>(IOReturnStatus.Fail, null, new Exception($"User '{userID}' was not found."));
@@ -80,46 +78,68 @@ namespace CovidTracker.Code.Database
             }
         }
 
-        ///
-        ///
-        ///
-        ///
-        ///
-        /// <summary>
-        /// !!!!!!!!!TEMPORARY CODE DELETE WHEN DONE!!!!!!!
-        /// </summary>
-        ///
-        ///
-        ///
-        ///
-        /// 
-        public static async void Test()
+        public static async Task<IOReturn<User>> RetrieveLocallyStoredUser(int userID)
         {
             using (IServiceScope scope = Program.AppHost.Services.CreateScope()) {
                 IServiceProvider services = scope.ServiceProvider;
                 DatabaseContext context = services.GetRequiredService<DatabaseContext>();
 
-                context.Users.Add(new User {
-                    UserID = 12345, 
-                    Name = "Joe", 
-                    AddressLine1 = "Lot 15 meme road", 
-                    AddressLine2 = "Big road", 
-                    PhoneNo = "2346759834", 
-                    Postcode = "2544", 
-                    Suburb = "bruh"
-                });
+                User user = await context.Users.FindAsync(userID);
+
+                return user == null 
+                    ? new IOReturn<User>(IOReturnStatus.Fail, null) 
+                    : new IOReturn<User>(IOReturnStatus.Success, user);
+            }
+        }
+
+        // !!!!!!!!!TEMPORARY CODE DELETE WHEN DONE!!!!!!!
+    #if DEBUG
+        public static async void Test()
+        {
+            // Create example data if it doesn't exist.
+            IOReturn<User> ret = await RetrieveLocallyStoredUser(1111);
+            if (ret.Status == IOReturnStatus.Fail) {
+                CreateExampleData();
+            }
+
+            // TODO: More tests to retrieve data.
+        }
+
+        public static async void CreateExampleData()
+        {
+            using (IServiceScope scope = Program.AppHost.Services.CreateScope()) {
+                IServiceProvider services = scope.ServiceProvider;
+                DatabaseContext context = services.GetRequiredService<DatabaseContext>();
+
+                context.Users.AddRange(
+                    new User { UserID = 1111, Name = "Joe", AddressLine1 = "Lot 15 meme road", AddressLine2 = "Big road", PhoneNo = "2346759834", Postcode = "2544", Suburb = "bruh"}, 
+                    new User { UserID = 1112, Name = "Bob", AddressLine1 = "Lot 17 road", AddressLine2 = "Small road", PhoneNo = "35634653", Postcode = "3456", Suburb = "Hello"},
+                    new User { UserID = 1113, Name = "Sally", AddressLine1 = "Road", AddressLine2 = "Medium road", PhoneNo = "53635523", Postcode = "4444", Suburb = "POtato"},
+                    new User { UserID = 1114, Name = "Worm", AddressLine1 = "Road2", AddressLine2 = "another road", PhoneNo = "63435244", Postcode = "4445", Suburb = "Dunno" },
+                    new User { UserID = 1115, Name = "Alan", AddressLine1 = "Road54", AddressLine2 = "getrgertrw", PhoneNo = "253245542", Postcode = "1029", Suburb = "Suburbia" }
+                    );
                 context.SaveChanges();
             }
 
-            IOReturn<User> ret = await RetrieveUser(12345);
-            User joe = ret.Value;
-
-            IOReturn ret2 = await RecordSignin(12345, DateTime.Now, new TerminalInfo {
-                AddressLine1 = "Town",
-                AddressLine2 = "THE town",
-                Suburb = "AAAA",
-                Postcode = "7777"
+            await RecordSignin(1111, DateTime.Now, new TerminalInfo {
+                AddressLine1 = "Town", AddressLine2 = "THE town", Suburb = "AAAA", Postcode = "7777"
+            });
+            await RecordSignin(1115, DateTime.Now, new TerminalInfo {
+                AddressLine1 = "Town", AddressLine2 = "THE town", Suburb = "AAAA", Postcode = "7777"
+            });
+            await RecordSignin(1112, DateTime.Now, new TerminalInfo {
+                AddressLine1 = "Town", AddressLine2 = "THE town", Suburb = "AAAA", Postcode = "7777"
+            });
+            await RecordSignin(1114, DateTime.Now, new TerminalInfo {
+                AddressLine1 = "Town", AddressLine2 = "THE town", Suburb = "AAAA", Postcode = "7777"
+            });
+            await RecordSignin(1113, DateTime.Now, new TerminalInfo {
+                AddressLine1 = "Town", AddressLine2 = "THE town", Suburb = "AAAA", Postcode = "7777"
+            });
+            await RecordSignin(1111, DateTime.Now, new TerminalInfo {
+                AddressLine1 = "Other town", AddressLine2 = "not the town", Suburb = "Three", Postcode = "4628"
             });
         }
+    #endif
     }
 }
