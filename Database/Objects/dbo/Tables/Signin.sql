30159898f7c9501f9c119e9568c62b14976dc9f3
ZIHGY001 <89966525+ZIHGY001@users.noreply.github.com>
2021-09-14 21:13:58 +0930

    Add files via upload

---
+++ Database/Objects/dbo/Tables/Signin.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[Signin]
+(
+	SigninID int,
+   UserID int,
+   Time DateTime NOT NULL,
+   PhoneNo varchar(100),
+   AddressLine1 varchar(100),
+   AddressLine2 varchar(100),
+   Suburb varchar(100),
+   Postcode varchar(100),
+   CONSTRAINT SigninID PRIMARY KEY(SigninID),
+   CONSTRAINT UserIDFK FOREIGN KEY(UserID)REFERENCES Users(UserID)
+
+)
