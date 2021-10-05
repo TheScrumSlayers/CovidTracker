5da44b1369e2505c7c78bb3539e431982e4cba36
Gitspathe <55875553+Gitspathe@users.noreply.github.com>
2021-10-05 20:59:00 +1100

    Testing & fixes

--- WebApp/Code/Database/DatabaseHelper.cs
+++ WebApp/Code/Database/DatabaseHelper.cs
@@ -21,7 +21,7 @@ namespace CovidTracker.Code.Database
                 DatabaseContext context = services.GetRequiredService<DatabaseContext>();
 
                 foreach (User user in users) {                    
-                    IOReturn<HashSet<User>> ret = await GetContactedUsers(user, depth, beforeDate, afterDate);
+                    IOReturn<HashSet<User>> ret = await GetContactedUsers(scope, user, depth, beforeDate, afterDate);
                     HashSet<User> tmp = ret.Value;
                     reportUsers.AddRange(tmp);
                 }
@@ -30,45 +30,41 @@ namespace CovidTracker.Code.Database
             return new IOReturn<List<User>>(IOReturnStatus.Success, reportUsers.ToList());
         }
 
-        private static async Task<IOReturn<HashSet<User>>> GetContactedUsers(User user, int depth, DateTime beforeDate, DateTime afterDate)
+        private static async Task<IOReturn<HashSet<User>>> GetContactedUsers(IServiceScope scope, User user, int depth, DateTime beforeDate, DateTime afterDate)
         {
             HashSet<User> reportUsers = new HashSet<User>();
-            using (IServiceScope scope = Program.AppHost.Services.CreateScope()) {
-                IServiceProvider services = scope.ServiceProvider;
-                DatabaseContext context = services.GetRequiredService<DatabaseContext>();
+            IServiceProvider services = scope.ServiceProvider;
+            DatabaseContext context = services.GetRequiredService<DatabaseContext>();
 
-                // Iterate over the locations where the person has visited.
-                IEnumerable<Signin> signinsEnumerable = context.Signins;
-                List<Signin> signins = signinsEnumerable.Where(s => s.UserID == user.UserID && s.Time <= beforeDate && s.Time >= afterDate).ToList();
-                foreach(Signin signin in signins) {
-                    string adrLine1 = signin.AddressLine1;
-                    string adrLine2 = signin.AddressLine2;
-                    string suburb = signin.Suburb;
-                    string code = signin.Postcode;
+            // Iterate over the locations where the person has visited.
+            List<Signin> signins = context.Signins.Where(s => s.UserID == user.UserID && s.Time <= beforeDate && s.Time >= afterDate).ToList();
+            foreach(Signin signin in signins) {
+                string adrLine1 = signin.AddressLine1;
+                string adrLine2 = signin.AddressLine2;
+                string suburb = signin.Suburb;
+                string code = signin.Postcode;
 
-                    User reported = user;
-                    reportUsers.Add(reported);
+                User reported = user;
+                reportUsers.Add(reported);
 
-                    // If depth is still > 0, iterate over each location recursively.
-                    if (depth > 0) {
-                        IEnumerable<Signin> signinsContactEnumerable = context.Signins;
-                        List<Signin> signinsContact = signinsContactEnumerable.Where(
-                            s => s.AddressLine1 == adrLine1
-                            && s.AddressLine2 == adrLine2
-                            && s.Suburb == suburb
-                            && s.Postcode == code 
-                            && s.Time <= beforeDate 
-                            && s.Time >= afterDate).ToList();
+                // If depth is still > 0, iterate over each location recursively.
+                if (depth > 0) {
+                    List<Signin> signinsContact = context.Signins.Where(
+                        s => s.AddressLine1 == adrLine1
+                        && s.AddressLine2 == adrLine2
+                        && s.Suburb == suburb
+                        && s.Postcode == code
+                        && s.Time <= beforeDate
+                        && s.Time >= afterDate).ToList();
 
-                        // Recursively report valid contacts until the depth reaches 0.
-                        foreach(Signin contact in signinsContact) {
-                            User contactUser = context.Users.Where(u => u.UserID == contact.UserID).FirstOrDefault();
-                            reportUsers.Add(contactUser);
+                    // Recursively report valid contacts until the depth reaches 0.
+                    foreach(Signin contact in signinsContact) {
+                        User contactUser = context.Users.Where(u => u.UserID == contact.UserID).FirstOrDefault();
+                        reportUsers.Add(contactUser);
 
-                            IOReturn<HashSet<User>> ret = await GetContactedUsers(contactUser, depth - 1, beforeDate, afterDate);
-                            HashSet<User> tmp = ret.Value;
-                            reportUsers.AddRange(tmp);
-                        }
+                        IOReturn<HashSet<User>> ret = await GetContactedUsers(scope, contactUser, depth - 1, beforeDate, afterDate);
+                        HashSet<User> tmp = ret.Value;
+                        reportUsers.AddRange(tmp);
                     }
                 }
             }
@@ -209,46 +205,10 @@ namespace CovidTracker.Code.Database
             // Create example data if it doesn't exist.
             IOReturn<User> ret = await RetrieveLocallyStoredUser(1111);
             if (ret.Status == IOReturnStatus.Fail) {
-                await CreateExampleData();
+                await DatabaseTest.GenerateTestData();
             }
 
             // TODO: More tests to retrieve data.
-        }
-
-        public static async Task CreateExampleData()
-        {
-            using (IServiceScope scope = Program.AppHost.Services.CreateScope()) {
-                IServiceProvider services = scope.ServiceProvider;
-                DatabaseContext context = services.GetRequiredService<DatabaseContext>();
-
-                context.Users.AddRange(
-                    new User { UserID = 1111, Name = "Joe", AddressLine1 = "Lot 15 meme road", AddressLine2 = "Big road", PhoneNo = "2346759834", Postcode = "2544", Suburb = "bruh"}, 
-                    new User { UserID = 1112, Name = "Bob", AddressLine1 = "Lot 17 road", AddressLine2 = "Small road", PhoneNo = "35634653", Postcode = "3456", Suburb = "Hello"},
-                    new User { UserID = 1113, Name = "Sally", AddressLine1 = "Road", AddressLine2 = "Medium road", PhoneNo = "53635523", Postcode = "4444", Suburb = "POtato"},
-                    new User { UserID = 1114, Name = "Worm", AddressLine1 = "Road2", AddressLine2 = "another road", PhoneNo = "63435244", Postcode = "4445", Suburb = "Dunno" },
-                    new User { UserID = 1115, Name = "Alan", AddressLine1 = "Road54", AddressLine2 = "getrgertrw", PhoneNo = "253245542", Postcode = "1029", Suburb = "Suburbia" }
-                    );
-                context.SaveChanges();
-            }
-
-            await RecordSignin(1111, DateTime.Now, new TerminalInfo {
-                AddressLine1 = "Town", AddressLine2 = "THE town", Suburb = "AAAA", Postcode = "7777"
-            });
-            await RecordSignin(1115, DateTime.Now, new TerminalInfo {
-                AddressLine1 = "Town", AddressLine2 = "THE town", Suburb = "AAAA", Postcode = "7777"
-            });
-            await RecordSignin(1112, DateTime.Now, new TerminalInfo {
-                AddressLine1 = "Town", AddressLine2 = "THE town", Suburb = "AAAA", Postcode = "7777"
-            });
-            await RecordSignin(1114, DateTime.Now, new TerminalInfo {
-                AddressLine1 = "Town", AddressLine2 = "THE town", Suburb = "AAAA", Postcode = "7777"
-            });
-            await RecordSignin(1113, DateTime.Now, new TerminalInfo {
-                AddressLine1 = "Town", AddressLine2 = "THE town", Suburb = "AAAA", Postcode = "7777"
-            });
-            await RecordSignin(1111, DateTime.Now, new TerminalInfo {
-                AddressLine1 = "Other town", AddressLine2 = "not the town", Suburb = "Three", Postcode = "4628"
-            });
         }
     #endif
     }
--- WebApp/Code/Database/DatabaseTest.cs
+++ WebApp/Code/Database/DatabaseTest.cs
@@ -10,9 +10,28 @@ namespace CovidTracker.Code.Database
 {
     public static class DatabaseTest
     {
-        public static string[] UserNames = new string[] { 
-            "Tom Wills", "Jacob Orange", "Sally Potato", "Micheal Green", "Terry Igor", "Jeffery Jefferson",
-            "Nameless McNoName", "Bobby", "Anthony El' Yelangno", "Grace Greyhair", "Alice Lanwonder", "Jeff Bob Jr."
+        public static Random Rand = new Random();
+
+        public static string[] FirstNames = new string[] {
+            "Tom", "Jacob", "Sally", "Micheal", "Terry", "Jeffery",
+            "Nameless", "Bobby", "Anthony", "Grace", "Alice", "Jeff", 
+            "John", "Zezima", "Scarlet", "Greg", "Joe", "Alfred", "Karen",
+            "Jesse", "Jessie", "Jason", "Stacy", "Jack"
+        };
+
+        public static string[] MiddleNames = new string[] {
+            "Tom", "Jacob", "Sally", "Micheal", "Terry", "Jeffery",
+            "Nameless", "Bobby", "Anthony", "Grace", "Alice", "Jeff",
+            "John", "Zezima", "Scarlet", "Greg", "Joe", "Alfred", "Karen",
+            "Jesse", "Jessie", "Jason", "Stacy", "Jack"
+        };
+
+        public static string[] LastNames = new string[] {
+            "Wills", "Orange", "Potato", "Green", "Igor", "Jefferson",
+            "McNoName", "El' Yelangno", "Greyhair", "Lanwonder", "Bob Jr.",
+            "Big", "Smalls", "Blue", "Emerald", "Longbeard", "Island",
+            "Jr.", "Oop", "Welsh", "Swims", "Walks", "Crawls", "Jogs",
+            "Carn", "Barner", "Beans"
         };
 
         public static string[] AddressLine1s = new string[] {
@@ -27,12 +46,69 @@ namespace CovidTracker.Code.Database
             "Mawson lakes", "Adelaide", "Birdwood", "mt. pleasant"
         };
 
-        public static void GenerateTestData() 
+        public static async Task GenerateTestData() 
         {
             using (IServiceScope scope = Program.AppHost.Services.CreateScope()) {
                 IServiceProvider services = scope.ServiceProvider;
-                DatabaseContext context = services.GetRequiredService<DatabaseContext>(); 
+                DatabaseContext context = services.GetRequiredService<DatabaseContext>();
+
+                List<User> users = new List<User>();
+
+                // Create users.
+                int index = 0;
+                for(int i = 0; i < 100; i++) {
+                    string name = FirstNames[Rand.Next(0, FirstNames.Length)];
+                    if(Rand.Next(0, 100) > 20) {
+                        name += " " + MiddleNames[Rand.Next(0, MiddleNames.Length)];
+                    }
+                    name += " " + LastNames[Rand.Next(0, LastNames.Length)];
+
+                    User user = new User() {
+                        UserID = 1111+index,
+                        Name = name,
+                        PhoneNo = Rand.Next(1000000, 9999999).ToString(),
+                        AddressLine1 = AddressLine1s[Rand.Next(0, AddressLine1s.Length)],
+                        AddressLine2 = AddressLine2s[Rand.Next(0, AddressLine2s.Length)],
+                        Suburb = Suburbs[Rand.Next(0, Suburbs.Length)],
+                        Postcode = Rand.Next(1000, 5000).ToString()
+                    };
+                    users.Add(user);
+                    await context.Users.AddAsync(user);
+                    ++index;
+                }
+                await context.SaveChangesAsync();
+
+                for (int i = 0; i < 30; i++) {
+                    string addressLine1 = AddressLine1s[Rand.Next(0, AddressLine1s.Length)];
+                    string addressLine2 = AddressLine2s[Rand.Next(0, AddressLine2s.Length)];
+                    string suburb = Suburbs[Rand.Next(0, Suburbs.Length)];
+                    string postcode = Rand.Next(1000, 5000).ToString();
+                    string phone = Rand.Next(1000000, 9999999).ToString();
+
+                    int amt = Rand.Next(1, 10);
+                    for(int y = 0; y < amt; y++) {
+                        Signin signin = new Signin() {
+                            UserID = users[Rand.Next(0, users.Count-1)].UserID,
+                            Time = RandomDateTime(),
+                            PhoneNo = phone,
+                            AddressLine1 = addressLine1,
+                            AddressLine2 = addressLine2,
+                            Suburb = suburb,
+                            Postcode = postcode
+                        };
+
+                        await context.Signins.AddAsync(signin);
+                    }
+                }
+                await context.SaveChangesAsync();
             }
+        }
+
+        public static DateTime RandomDateTime()
+        {
+            DateTime start = new DateTime(1995, 1, 1);
+            int range = (DateTime.Today - start).Days;
+            return start.AddDays(Rand.Next(range));
         }
 
     }
